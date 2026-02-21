af7f3aec26309a08f76686dd663bd299ac8bd573
Rostyslav Ugryniuk <ug.ross@hotmail.com>
2026-02-21 12:10:45 +0100

    Update createExcerpt.ts

--- src/utils/createExcerpt.ts
+++ src/utils/createExcerpt.ts
@@ -2,21 +2,21 @@ import { convert } from 'html-to-text'
 import MarkdownIt from 'markdown-it'
 
 const parser = new MarkdownIt()
+const HTML_TO_TEXT_OPTIONS = {
+  selectors: [
+    { options: { ignoreHref: true }, selector: 'a' },
+    { format: 'skip', selector: 'img' },
+    { format: 'skip', selector: 'figure' },
+    { format: 'skip', selector: 'pre' },
+    { format: 'skip', selector: 'code' },
+    { format: 'skip', selector: 'hr' }
+  ],
+  wordwrap: null
+}
 
 export const createExcerpt = (body: string): string => {
   const html = parser.render(body)
-  const options = {
-    selectors: [
-      { options: { ignoreHref: true }, selector: 'a' },
-      { format: 'skip', selector: 'img' },
-      { format: 'skip', selector: 'figure' },
-      { format: 'skip', selector: 'pre' },
-      { format: 'skip', selector: 'code' },
-      { format: 'skip', selector: 'hr' }
-    ],
-    wordwrap: null
-  }
 
-  const text = convert(html, options)
+  const text = convert(html, HTML_TO_TEXT_OPTIONS)
   return text.trim()
 }
